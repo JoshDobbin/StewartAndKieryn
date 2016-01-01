cb5dfe15a6855c57eba5e9f2443b514523df2499
Josh Dobbin <josh@dobbinconsulting.com>
2016-01-01 09:17:32 -0700

    Added coin counter!

--- Assets/scripts/pickUpCoin.cs
+++ Assets/scripts/pickUpCoin.cs
@@ -3,19 +3,22 @@ using System.Collections;
 
 public class pickUpCoin : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	void OnTriggerEnter2D (Collider2D other)
-	{
-		if (other.gameObject.CompareTag ("Player")) {
-			Destroy (gameObject);
-		}
-	}
+    public GameObject counter;
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.gameObject.CompareTag ("Player")) {
+            counter.BroadcastMessage("IncreaseCoinCount");
+            Destroy (gameObject);
+        }
+    }
 }
